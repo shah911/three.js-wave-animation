8469a43f9963b46320bab35451d1d7aa2d6bfe55
shah911 <mianshahzeb911@gmail.com>
2024-08-18 19:13:41 +0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Sence from "@/components/Sence";
 
 export default function Home() {
   return (
-    <main className="h-screen">
+    <main className="min-h-[600px] h-screen">
       <Sence />
     </main>
   );
